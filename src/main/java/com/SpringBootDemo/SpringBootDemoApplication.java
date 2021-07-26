a1d57a5c40fae9764e0b256f373509d7fe576fc9
Chen Yijun <yijunchenchn@outlook.com>
2021-07-26 09:49:43 +0800

    解决ie数据保存出现下载对话框的问题

--- src/main/java/com/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/SpringBootDemo/SpringBootDemoApplication.java
@@ -3,9 +3,18 @@
  */
 package com.SpringBootDemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 /**
  * @author yijun
@@ -14,6 +23,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.SpringBootDemo.dao")
 public class SpringBootDemoApplication {
+
+	/**
+	 * 解决ie数据保存出现下载对话框的问题
+	 * 
+	 * @return
+	 */
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		// 1.需要定义一个convert转换消息的对象;
+		FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+		// 2处理ie浏览器保存数据时出现下载json数据问题
+		List<MediaType> fastMediaTypes = new ArrayList<>();
+		fastMediaTypes.add(MediaType.TEXT_PLAIN);
+		// 3.在convert中添加配置信息.
+		fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+		HttpMessageConverter<?> converter = fastJsonHttpMessageConverter;
+		return new HttpMessageConverters(converter);
+	}
 
 	/**
 	 * @param args
