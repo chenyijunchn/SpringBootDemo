ce0d4f7f4ec003cfe23932bb7d612d2af96691e3
Chen Yijun <yijunchenchn@outlook.com>
2021-08-19 17:29:20 +0800

    add banner.txt

---
+++ chapter04/BannerDemo/src/main/java/com/example/bannerdemo/BannerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bannerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BannerDemoApplication.class, args);
+    }
+
+}
---
+++ chapter04/BannerDemo/src/test/java/com/example/bannerdemo/BannerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bannerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
