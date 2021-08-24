4e76ccde7160be14d5b302d579c6bf5ada8fd2e9
Chen Yijun <yijunchenchn@outlook.com>
2021-08-24 17:14:09 +0800

    chapter 05 实例8 表单数据自定义验证

--- chapter05/src/main/java/com/example/mvcdemo/model/User.java
+++ chapter05/src/main/java/com/example/mvcdemo/model/User.java
@@ -1,7 +1,15 @@
 package com.example.mvcdemo.model;
 
+import com.example.mvcdemo.MyConstraint;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.Length;
 /**
  * FileName:User
  * Author:Jane
@@ -9,8 +17,18 @@ import lombok.Data;
  * Description:User Entity
  */
 @Data
-public class User {
+public class User implements Serializable {
     private long id;
+
+    @NotBlank(message="user name cannot be null")
+    @Length(min=1,max=20)
     private String name;
+
+    @NotNull(message="age cannot be null")
+    @Min(value = 0)
+    @Max(value = 120)
     private int age;
+
+    @MyConstraint
+    private String answer;
 }
