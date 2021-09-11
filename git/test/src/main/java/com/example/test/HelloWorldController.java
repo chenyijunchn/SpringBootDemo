c77757aa90e64dcd3bd88d1a5e29218c9a4af79d
Chen Yijun <yijunchenchn@outlook.com>
2021-09-11 15:57:39 +0800

    add

---
+++ git/test/src/main/java/com/example/test/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping ("/hello")
+    public String hello(){
+        return "Hello, Spring Boot";
+    }
+
+
+    public void dev(){
+        System.out.println("liwenhao");
+    }
+}
